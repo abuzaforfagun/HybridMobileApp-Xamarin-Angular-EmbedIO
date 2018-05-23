2708b35b43fde01235c3b3e18213a9961ff203d8
abuzaforfagun <mdabuzaforfagun@gmail.com>
2018-05-23 18:04:19 +0600

    Implement one end point

---
+++ XamarinApp/XamarinApp/XamarinApp.iOS/MyFavouriteProgrammingLanguage.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Unosquare.Labs.EmbedIO;
+using Unosquare.Labs.EmbedIO.Constants;
+using Unosquare.Labs.EmbedIO.Modules;
+
+namespace XamarinApp.iOS
+{
+    public class MyFavouriteProgrammingLanguage : WebApiController
+    {
+        [WebApiHandler(HttpVerbs.Get, "/api/favouritelanguage")]
+        public bool GetAll(WebServer server,
+                            HttpListenerContext context)
+        {
+            var languageList = new List<string>();
+            languageList.Add("C#");
+            languageList.Add("TypeScript");
+
+            return context.JsonResponse(new
+            {
+                list = languageList.ToArray()
+            });
+            
+        }
+    }
+}
