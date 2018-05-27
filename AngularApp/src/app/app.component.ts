03defb87717c0eca2fc76ef1a5f81d42ae4b2fdd
abuzaforfagun <mdabuzaforfagun@gmail.com>
2018-05-27 14:00:18 +0600

    Remove unused codes

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,25 +7,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-  title = 'app';
   languageList: any;
   constructor(private http: HttpClient) {
     this.languageList = [];
   }
   ngOnInit(): void {
-    let headers = new HttpHeaders();
-    headers.append('Content-Type','application/json');
-    headers.append('Accept', 'application/json');
-    headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT');
-    headers.append('Access-Control-Allow-Origin', '*');
-
-    this.http.get('http://localhost:8080/api/favouritelanguage', {
-      headers: headers
-    } ).subscribe(result => {
+    this.http.get('http://localhost:8080/api/favouritelanguage').subscribe(result => {
       this.languageList = result;
     });
-    // this.languageList.push("C#");
-    // this.languageList.push("TypeScript")
   }
 }
